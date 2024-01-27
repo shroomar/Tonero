023fe93a04f8d1efd9ecc0d98494c18519603aa2
shroomar <omar.ai@skiff.com>
2024-01-27 12:07:50 +0100

    split Serializer to a separate file

--- C#/MPM.cs
+++ C#/MPM.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Text;
 using System.Collections.Generic;
-using System.IO;
-using System.Xml.Serialization;
 
 namespace Tonero
 {
@@ -41,7 +38,12 @@ namespace Tonero
         }
         public static MPM Import(string read)
         {
-            return SerializationHelper.DeserializeXml<MPM>(read);
+            return Serializer.DeserializeXml<MPM>(read);
+        }
+        public static MPM Import(string[] parts,int rem)
+        {
+            string read = Serializer.Join(parts, rem);
+            return Serializer.DeserializeXml<MPM>(read);
         }
         public void Add_DeleteTag(string t, bool original)
         {
@@ -72,55 +74,19 @@ namespace Tonero
         }
         public string Export()
         {
-            return SerializationHelper.SerializeXml(this);
+            return Serializer.SerializeXml(this);
+        }
+        public (int, string[]) Export(int limit)
+        {
+            string all = Serializer.SerializeXml(this);
+            return Serializer.Split(all, limit);
+        }
+        public static bool IsMPM(string str)
+        {
+            return str.StartsWith(Serializer.ToHexString(Serializer.Prefix));
         }
     }
 
 
-    internal static class SerializationHelper
-    {
-        public static string Prefix = "This is an MPM : ";
-        public static T DeserializeXml<T>(this string toDeserialize)
-        {
-            string xml = FromHexString(toDeserialize);
-            xml = xml.Substring(Prefix.Length);
-            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
-            using (StringReader textReader = new StringReader(xml))
-            {
-                return (T)xmlSerializer.Deserialize(textReader);
-            }
-        }
-        public static string SerializeXml<T>(this T toSerialize)
-        {
-            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
-            using (StringWriter textWriter = new StringWriter())
-            {
-                xmlSerializer.Serialize(textWriter, toSerialize);
-                string text = Prefix + textWriter.ToString();
-                return ToHexString(text);
-            }
-        }
-        private static string ToHexString(string str)
-        {
-            var sb = new StringBuilder();
-
-            var bytes = Encoding.Unicode.GetBytes(str);
-            foreach (var t in bytes)
-            {
-                sb.Append(t.ToString("X2"));
-            }
-
-            return sb.ToString(); // returns: "48656C6C6F20776F726C64" for "Hello world"
-        }
-        private static string FromHexString(string hexString)
-        {
-            var bytes = new byte[hexString.Length / 2];
-            for (var i = 0; i < bytes.Length; i++)
-            {
-                bytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
-            }
-
-            return Encoding.Unicode.GetString(bytes); // returns: "Hello world" for "48656C6C6F20776F726C64"
-        }
-    }
+    
 }
---
+++ C#/Serializer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Tonero
+{
+    public static class Serializer
+    {
+        public static string Prefix = "This is an MPM : ";
+        public static T DeserializeXml<T>(this string toDeserialize)
+        {
+            string xml = FromHexString(toDeserialize);
+            xml = xml.Substring(Prefix.Length);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            using (StringReader textReader = new StringReader(xml))
+            {
+                return (T)xmlSerializer.Deserialize(textReader);
+            }
+        }
+        public static string SerializeXml<T>(this T toSerialize)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, toSerialize);
+                string text = Prefix + textWriter.ToString();
+                return ToHexString(text);
+            }
+        }
+        public static string ToHexString(string str)
+        {
+            var sb = new StringBuilder();
+
+            var bytes = Encoding.Unicode.GetBytes(str);
+            foreach (var t in bytes)
+            {
+                sb.Append(t.ToString("X2"));
+            }
+
+            return sb.ToString(); // returns: "48656C6C6F20776F726C64" for "Hello world"
+        }
+        public static string FromHexString(string hexString)
+        {
+            var bytes = new byte[hexString.Length / 2];
+            for (var i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+            }
+
+            return Encoding.Unicode.GetString(bytes); // returns: "Hello world" for "48656C6C6F20776F726C64"
+        }
+        public static (int, string[]) Split(string str, int limit)
+        {
+            int p = 0; int c = 0;
+            string[] parts = new string[str.Length / limit + 1];
+            for (int i = 0; i < str.Length; i++)
+            {
+                parts[p] += str[i];
+                c++;
+                if (c == limit)
+                { p++; c = 0; }
+            }
+            int rem = parts.Length * limit - str.Length;
+            for (int j = 0; j < rem; j++)
+                parts[parts.Length - 1] += "0";
+            return (rem, parts);
+        }
+        public static string Join(string[] parts, int rem)
+        {
+            string all = "";
+            for (int i = 0; i < parts.Length; i++)
+                all += parts[i];
+            return all.Substring(0, all.Length - rem);
+        }
+    }
+}
